84ed94d84d9946aa1ce579fadcc8bec6bffed843
Wisnu Nugroho <wisnunugroho614@gmail.com>
2025-04-23 05:45:21 +0700

    Initial publisher code.

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn get_handler_action(&self) -> String {
+        "user_created".to_string()
+    }
+
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!("Message received on handler 1: {:?}", message);
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut p =
+        CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+
+    _ = p.publish_event(
+        "user_created".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "1".to_owned(),
+            user_name: "2306275084-Amir".to_owned(),
+        },
+    );
+    _ = p.publish_event(
+        "user_created".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "2".to_owned(),
+            user_name: "2306275084-Budi".to_owned(),
+        },
+    );
+    _ = p.publish_event(
+        "user_created".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "3".to_owned(),
+            user_name: "2306275084-Cica".to_owned(),
+        },
+    );
+    _ = p.publish_event(
+        "user_created".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "4".to_owned(),
+            user_name: "2306275084-Dira".to_owned(),
+        },
+    );
+    _ = p.publish_event(
+        "user_created".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "5".to_owned(),
+            user_name: "2306275084-Emir".to_owned(),
+        },
+    );
+}
